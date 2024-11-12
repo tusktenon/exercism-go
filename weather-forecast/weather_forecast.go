c14f67ad83cfc13ffcfe72f9a91a3c65362419b1
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-12 09:04:11 -0500

    Weather Forecast: complete

--- weather-forecast/weather_forecast.go
+++ weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather provides the current weather conditions at various locations.
 package weather
 
+// CurrentCondition represents the current weather conditions at a specific location.
 var CurrentCondition string
+
+// CurrentLocation represents a city whose current weather conditions are of interest.
 var CurrentLocation string
 
+// Forecast returns a string describing the given city and its current weather conditions.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
