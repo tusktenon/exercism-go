a5ec454a485682ef319ac1af57115a0de6931816
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-12 18:01:44 -0500

    Vehicle Purchase: complete

--- vehicle-purchase/vehicle_purchase.go
+++ vehicle-purchase/vehicle_purchase.go
@@ -2,15 +2,29 @@ package purchase
 
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	panic("NeedsLicense not implemented")
+	return kind == "car" || kind == "truck"
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+    var first string
+	if option2 < option1 {
+		first = option2
+	} else {
+        first = option1
+    }
+	return first + " is clearly the better choice."
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
-	panic("CalculateResellPrice not implemented")
+	var percent float64
+	if age < 3 {
+		percent = 80
+	} else if age < 10 {
+		percent = 70
+	} else {
+		percent = 50
+	}
+	return originalPrice * percent / 100
 }
