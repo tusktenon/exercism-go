5918815727f7f486c39fc3a7e63b0b0303a1cfea
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-13 11:05:16 -0500

    Blackjack: complete

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -2,11 +2,49 @@ package blackjack
 
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
-	panic("Please implement the ParseCard function")
+	switch card {
+	case "ace":
+		return 11
+	case "two":
+		return 2
+	case "three":
+		return 3
+	case "four":
+		return 4
+	case "five":
+		return 5
+	case "six":
+		return 6
+	case "seven":
+		return 7
+	case "eight":
+		return 8
+	case "nine":
+		return 9
+	case "ten", "jack", "queen", "king":
+		return 10
+	default:
+		return 0
+	}
 }
 
 // FirstTurn returns the decision for the first turn, given two cards of the
 // player and one card of the dealer.
 func FirstTurn(card1, card2, dealerCard string) string {
-	panic("Please implement the FirstTurn function")
+	p := ParseCard(card1) + ParseCard(card2)
+	d := ParseCard(dealerCard)
+	switch {
+	case p == 22:
+		return "P"
+	case p == 21 && d < 10:
+		return "W"
+	case p == 21:
+		return "S"
+	case p >= 17:
+		return "S"
+	case p >= 12 && d < 7:
+		return "S"
+	default:
+		return "H"
+	}
 }
