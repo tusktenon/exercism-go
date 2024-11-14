4ddc53f81baef5a708d39aa4059bd7455d60312f
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-14 10:46:23 -0500

    Card Tricks: complete

--- card-tricks/card_tricks.go
+++ card-tricks/card_tricks.go
@@ -2,27 +2,43 @@ package cards
 
 // FavoriteCards returns a slice with the cards 2, 6 and 9 in that order.
 func FavoriteCards() []int {
-	panic("Please implement the FavoriteCards function")
+	return []int{2, 6, 9}
 }
 
 // GetItem retrieves an item from a slice at given position.
 // If the index is out of range, we want it to return -1.
 func GetItem(slice []int, index int) int {
-	panic("Please implement the GetItem function")
+	if inRange(slice, index) {
+		return slice[index]
+	}
+	return -1
 }
 
 // SetItem writes an item to a slice at given position overwriting an existing value.
 // If the index is out of range the value needs to be appended.
 func SetItem(slice []int, index, value int) []int {
-	panic("Please implement the SetItem function")
+	if inRange(slice, index) {
+		slice[index] = value
+	} else {
+		slice = append(slice, value)
+	}
+	return slice
 }
 
 // PrependItems adds an arbitrary number of values at the front of a slice.
 func PrependItems(slice []int, values ...int) []int {
-	panic("Please implement the PrependItems function")
+	return append(values, slice...)
 }
 
 // RemoveItem removes an item from a slice by modifying the existing slice.
 func RemoveItem(slice []int, index int) []int {
-	panic("Please implement the RemoveItem function")
+	if inRange(slice, index) {
+		slice = append(slice[:index], slice[index+1:]...)
+	}
+	return slice
+}
+
+// inRange returns true if i is a valid index into slice s.
+func inRange(s []int, i int) bool {
+	return 0 <= i && i < len(s)
 }
