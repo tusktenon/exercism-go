c17ab4559ab79246416f0e34f85dd318f3c520ed
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-15 15:30:57 -0500

    Bird Watcher: complete

--- bird-watcher/bird_watcher.go
+++ bird-watcher/bird_watcher.go
@@ -3,17 +3,25 @@ package birdwatcher
 // TotalBirdCount return the total bird count by summing
 // the individual day's counts.
 func TotalBirdCount(birdsPerDay []int) int {
-	panic("Please implement the TotalBirdCount() function")
+	total := 0
+	for _, count := range birdsPerDay {
+		total += count
+	}
+	return total
 }
 
 // BirdsInWeek returns the total bird count by summing
 // only the items belonging to the given week.
 func BirdsInWeek(birdsPerDay []int, week int) int {
-	panic("Please implement the BirdsInWeek() function")
+	start := 7 * (week - 1)
+	return TotalBirdCount(birdsPerDay[start : start+7])
 }
 
 // FixBirdCountLog returns the bird counts after correcting
 // the bird counts for alternate days.
 func FixBirdCountLog(birdsPerDay []int) []int {
-	panic("Please implement the FixBirdCountLog() function")
+	for i := 0; i < len(birdsPerDay); i += 2 {
+		birdsPerDay[i]++
+	}
+	return birdsPerDay
 }
