1dfd4b9505850e274f2dcf595e4ea412f1b9bcba
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-14 22:25:07 -0500

    Need For Speed: complete

--- need-for-speed/need_for_speed.go
+++ need-for-speed/need_for_speed.go
@@ -1,26 +1,39 @@
 package speed
 
-// TODO: define the 'Car' type struct
+// Car represents a remote controlled car.
+type Car struct {
+	battery      int
+	batteryDrain int
+	speed        int
+	distance     int
+}
 
 // NewCar creates a new remote controlled car with full battery and given specifications.
 func NewCar(speed, batteryDrain int) Car {
-	panic("Please implement the NewCar function")
+	return Car{battery: 100, batteryDrain: batteryDrain, speed: speed}
 }
 
-// TODO: define the 'Track' type struct
+// Track represents a race track for remote controlled cars.
+type Track struct {
+	distance int
+}
 
-// NewTrack creates a new track
+// NewTrack creates a new track.
 func NewTrack(distance int) Track {
-	panic("Please implement the NewTrack function")
+	return Track{distance: distance}
 }
 
 // Drive drives the car one time. If there is not enough battery to drive one more time,
 // the car will not move.
 func Drive(car Car) Car {
-	panic("Please implement the Drive function")
+	if car.battery >= car.batteryDrain {
+		car.battery -= car.batteryDrain
+		car.distance += car.speed
+	}
+	return car
 }
 
 // CanFinish checks if a car is able to finish a certain track.
 func CanFinish(car Car, track Track) bool {
-	panic("Please implement the CanFinish function")
+	return car.speed*car.battery/car.batteryDrain >= track.distance
 }
