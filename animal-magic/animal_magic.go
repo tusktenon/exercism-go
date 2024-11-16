28f5bc20051ece8954adbd0caaaa2f563cacce74
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-16 15:28:40 -0500

    Animal Magic: complete

--- animal-magic/animal_magic.go
+++ animal-magic/animal_magic.go
@@ -1,16 +1,20 @@
 package chance
 
+import "math/rand"
+
 // RollADie returns a random int d with 1 <= d <= 20.
 func RollADie() int {
-	panic("Please implement the RollADie function")
+	return 1 + rand.Intn(20)
 }
 
 // GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
 func GenerateWandEnergy() float64 {
-	panic("Please implement the GenerateWandEnergy function")
+	return 12 * rand.Float64()
 }
 
 // ShuffleAnimals returns a slice with all eight animal strings in random order.
 func ShuffleAnimals() []string {
-	panic("Please implement the ShuffleAnimals function")
+	animals := []string{"ant", "beaver", "cat", "dog", "elephant", "fox", "giraffe", "hedgehog"}
+	rand.Shuffle(len(animals), func(i, j int) { animals[i], animals[j] = animals[j], animals[i] })
+	return animals
 }
