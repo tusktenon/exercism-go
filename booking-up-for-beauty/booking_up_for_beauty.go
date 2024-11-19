18e917718926c1ba87b019e8c99db48ab35d9eec
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-19 09:13:52 -0500

    Booking up for Beauty: complete

--- booking-up-for-beauty/booking_up_for_beauty.go
+++ booking-up-for-beauty/booking_up_for_beauty.go
@@ -4,25 +4,28 @@ import "time"
 
 // Schedule returns a time.Time from a string containing a date.
 func Schedule(date string) time.Time {
-	panic("Please implement the Schedule function")
+	t, _ := time.Parse("1/2/2006 15:04:05", date)
+	return t
 }
 
 // HasPassed returns whether a date has passed.
 func HasPassed(date string) bool {
-	panic("Please implement the HasPassed function")
+	d, _ := time.Parse("January 2, 2006 15:04:05", date)
+	return d.Before(time.Now())
 }
 
 // IsAfternoonAppointment returns whether a time is in the afternoon.
 func IsAfternoonAppointment(date string) bool {
-	panic("Please implement the IsAfternoonAppointment function")
+	d, _ := time.Parse("Monday, January 2, 2006 15:04:05", date)
+	return 12 <= d.Hour() && d.Hour() < 18
 }
 
 // Description returns a formatted string of the appointment time.
 func Description(date string) string {
-	panic("Please implement the Description function")
+	return Schedule(date).Format("You have an appointment on Monday, January 2, 2006, at 15:04.")
 }
 
 // AnniversaryDate returns a Time with this year's anniversary.
 func AnniversaryDate() time.Time {
-	panic("Please implement the AnniversaryDate function")
+	return time.Date(time.Now().Year(), 9, 15, 0, 0, 0, 0, time.UTC)
 }
