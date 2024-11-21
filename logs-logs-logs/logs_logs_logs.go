f83e6f4c2c972b19f327e5e733653175f05c4ce9
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-20 18:56:23 -0800

    Logs Logs Logs: first iteration

--- logs-logs-logs/logs_logs_logs.go
+++ logs-logs-logs/logs_logs_logs.go
@@ -1,18 +1,39 @@
 package logs
 
+import "unicode/utf8"
+
 // Application identifies the application emitting the given log.
 func Application(log string) string {
-	panic("Please implement the Application() function")
+	for _, r := range log {
+		switch r {
+		case '❗':
+			return "recommendation"
+		case '🔍':
+			return "search"
+		case '☀':
+			return "weather"
+		}
+	}
+	return "default"
 }
 
 // Replace replaces all occurrences of old with new, returning the modified log
 // to the caller.
+// NOTE: We could also just use the strings.ReplaceAll function.
 func Replace(log string, oldRune, newRune rune) string {
-	panic("Please implement the Replace() function")
+	rs := make([]rune, 0, utf8.RuneCountInString(log))
+	for _, r := range log {
+		if r == oldRune {
+			rs = append(rs, newRune)
+		} else {
+			rs = append(rs, r)
+		}
+	}
+	return string(rs)
 }
 
 // WithinLimit determines whether or not the number of characters in log is
 // within the limit.
 func WithinLimit(log string, limit int) bool {
-	panic("Please implement the WithinLimit() function")
+	return utf8.RuneCountInString(log) <= limit
 }
