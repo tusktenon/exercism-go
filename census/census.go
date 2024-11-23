dd7ac0735f706c2934b486342ae6339c6ef12b25
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-22 20:57:56 -0800

    Census: complete

--- census/census.go
+++ census/census.go
@@ -10,20 +10,25 @@ type Resident struct {
 
 // NewResident registers a new resident in this city.
 func NewResident(name string, age int, address map[string]string) *Resident {
-	panic("Please implement NewResident.")
+	return &Resident{Name: name, Age: age, Address: address}
 }
 
 // HasRequiredInfo determines if a given resident has all of the required information.
 func (r *Resident) HasRequiredInfo() bool {
-	panic("Please implement HasRequiredInfo.")
+	return r.Name != "" && r.Address["street"] != ""
 }
 
 // Delete deletes a resident's information.
 func (r *Resident) Delete() {
-	panic("Please implement Delete.")
+	*r = Resident{}
 }
 
 // Count counts all residents that have provided the required information.
-func Count(residents []*Resident) int {
-	panic("Please implement Count.")
+func Count(residents []*Resident) (count int) {
+	for _, r := range residents {
+		if r.HasRequiredInfo() {
+			count++
+		}
+	}
+	return
 }
