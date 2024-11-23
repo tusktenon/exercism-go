572eb38f01a855e340aedecbc65233d733f10478
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-22 20:22:47 -0800

    Elon's Toys: complete

--- elons-toys/elons_toys.go
+++ elons-toys/elons_toys.go
@@ -1,18 +1,27 @@
 package elon
 
-// TODO: define the 'Drive()' method
+import "fmt"
 
-// TODO: define the 'DisplayDistance() string' method
+// Drive increases the car's distance driven by its speed and reduces its
+// battery according to the battery drainage.
+func (c *Car) Drive() {
+	if c.battery >= c.batteryDrain {
+		c.distance += c.speed
+		c.battery -= c.batteryDrain
+	}
+}
 
-// TODO: define the 'DisplayBattery() string' method
+// DisplayDistance returns the LED display for the distance driven.
+func (c Car) DisplayDistance() string {
+	return fmt.Sprintf("Driven %d meters", c.distance)
+}
 
-// TODO: define the 'CanFinish(trackDistance int) bool' method
+// DisplayBattery returns the LED display for the remaining battery charge.
+func (c Car) DisplayBattery() string {
+	return fmt.Sprintf("Battery at %d%%", c.battery)
+}
 
-// Your first steps could be to read through the tasks, and create
-// these functions with their correct parameter lists and return types.
-// The function body only needs to contain `panic("")`.
-// 
-// This will make the tests compile, but they will fail.
-// You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more 
-// functionality.
+// CanFinish returns true if the car can finish a race of the given distance.
+func (c Car) CanFinish(trackDistance int) bool {
+	return c.speed*(c.battery/c.batteryDrain) >= trackDistance
+}
