4ca02832a66327dd63c453301279550b5468f494
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-23 14:01:24 -0800

    Parsing Log Files: complete

--- parsing-log-files/parsing_log_files.go
+++ parsing-log-files/parsing_log_files.go
@@ -1,21 +1,38 @@
 package parsinglogfiles
 
+import (
+	"fmt"
+	"regexp"
+)
+
 func IsValidLine(text string) bool {
-	panic("Please implement the IsValidLine function")
+	return regexp.MustCompile(`^\[(DBG|ERR|FTL|INF|TRC|WRN)\] `).MatchString(text)
 }
 
 func SplitLogLine(text string) []string {
-	panic("Please implement the SplitLogLine function")
+	return regexp.MustCompile(`<[~*-=]*>`).Split(text, -1)
 }
 
-func CountQuotedPasswords(lines []string) int {
-	panic("Please implement the CountQuotedPasswords function")
+func CountQuotedPasswords(lines []string) (count int) {
+	re := regexp.MustCompile(`".*(?i)(password).*"`)
+	for _, line := range lines {
+		if re.MatchString(line) {
+			count++
+		}
+	}
+	return
 }
 
 func RemoveEndOfLineText(text string) string {
-	panic("Please implement the RemoveEndOfLineText function")
+	return regexp.MustCompile(`end-of-line\d+`).ReplaceAllString(text, "")
 }
 
 func TagWithUserName(lines []string) []string {
-	panic("Please implement the TagWithUserName function")
+	re := regexp.MustCompile(`\s*User \s*(\S+)`)
+	for i, line := range lines {
+		if match := re.FindStringSubmatch(line); match != nil {
+			lines[i] = fmt.Sprintf("[USR] %s %s", match[1], line)
+		}
+	}
+	return lines
 }
