684fd8edcd28ba28f4ca679a28f8aa0c42219fc8
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-24 10:18:42 -0800

    Airport Robot: complete

--- airport-robot/airport_robot.go
+++ airport-robot/airport_robot.go
@@ -1,5 +1,30 @@
 package airportrobot
 
-// Write your code here.
-// This exercise does not have tests for each individual task.
-// Try to solve all the tasks first before running the tests.
+type Greeter interface {
+	LanguageName() string
+	Greet(name string) string
+}
+
+func SayHello(name string, g Greeter) string {
+	return "I can speak " + g.LanguageName() + ": " + g.Greet(name)
+}
+
+type Italian struct{}
+
+func (i Italian) LanguageName() string {
+	return "Italian"
+}
+
+func (i Italian) Greet(name string) string {
+	return "Ciao " + name + "!"
+}
+
+type Portuguese struct{}
+
+func (p Portuguese) LanguageName() string {
+	return "Portuguese"
+}
+
+func (p Portuguese) Greet(name string) string {
+	return "Olá " + name + "!"
+}
