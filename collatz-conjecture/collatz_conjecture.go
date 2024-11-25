7deca0229dd4bdee928006192534677550152864
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-25 15:27:11 -0800

    Collatz Conjecture: second iteration
    Use bit operations instead of arithmetic ones for improved performance.

--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -8,8 +8,10 @@ func CollatzConjecture(n int) (int, error) {
 	}
 	s := 0
 	for ; n > 1; s++ {
-		if n%2 == 0 {
-			n = n / 2
+        // benchmarking shows a 10% performance improvement in using bit operations
+        // (n&1 and n >>= 1) over arithmetic ones (n%2 and n /= 2)
+		if n&1 == 0 {
+			n >>= 1
 		} else {
 			n = 3*n + 1
 		}
