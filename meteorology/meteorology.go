7593a22b42ea57083c9d846cf180729412f3bb45
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-24 18:36:42 -0800

    Meteorology: first iteration

--- meteorology/meteorology.go
+++ meteorology/meteorology.go
@@ -1,5 +1,7 @@
 package meteorology
 
+import "fmt"
+
 type TemperatureUnit int
 
 const (
@@ -7,14 +9,19 @@ const (
 	Fahrenheit TemperatureUnit = 1
 )
 
-// Add a String method to the TemperatureUnit type
+func (tu TemperatureUnit) String() string {
+	units := []string{"°C", "°F"}
+	return units[tu]
+}
 
 type Temperature struct {
 	degree int
 	unit   TemperatureUnit
 }
 
-// Add a String method to the Temperature type
+func (t Temperature) String() string {
+	return fmt.Sprintf("%v %v", t.degree, t.unit)
+}
 
 type SpeedUnit int
 
@@ -23,14 +30,19 @@ const (
 	MilesPerHour SpeedUnit = 1
 )
 
-// Add a String method to SpeedUnit
+func (su SpeedUnit) String() string {
+	units := []string{"km/h", "mph"}
+	return units[su]
+}
 
 type Speed struct {
 	magnitude int
 	unit      SpeedUnit
 }
 
-// Add a String method to Speed
+func (s Speed) String() string {
+	return fmt.Sprintf("%v %v", s.magnitude, s.unit)
+}
 
 type MeteorologyData struct {
 	location      string
@@ -40,4 +52,13 @@ type MeteorologyData struct {
 	humidity      int
 }
 
-// Add a String method to MeteorologyData
+func (md MeteorologyData) String() string {
+	return fmt.Sprintf(
+		"%v: %v, Wind %v at %v, %v%% Humidity",
+		md.location,
+		md.temperature,
+		md.windDirection,
+		md.windSpeed,
+		md.humidity,
+	)
+}
