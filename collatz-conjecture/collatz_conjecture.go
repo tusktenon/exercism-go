e6170be5e7af8576d5ecc63efa909c17e158033f
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-25 15:15:14 -0800

    Collatz Conjecture: first iteration

--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,18 @@
 package collatzconjecture
 
+import "fmt"
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n < 1 {
+		return 0, fmt.Errorf("non-positive input %d", n)
+	}
+	s := 0
+	for ; n > 1; s++ {
+		if n%2 == 0 {
+			n = n / 2
+		} else {
+			n = 3*n + 1
+		}
+	}
+	return s, nil
 }
