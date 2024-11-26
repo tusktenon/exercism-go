31087c09d043f71d169a59b53ed83f4e6817d87a
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-25 18:03:54 -0800

    Scrabble Score: complete

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -1,5 +1,25 @@
 package scrabble
 
-func Score(word string) int {
-	panic("Please implement the Score function")
+import "unicode"
+
+func Score(word string) (score int) {
+	for _, r := range word {
+		switch unicode.ToUpper(r) {
+		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+			score += 1
+		case 'D', 'G':
+			score += 2
+		case 'B', 'C', 'M', 'P':
+			score += 3
+		case 'F', 'H', 'V', 'W', 'Y':
+			score += 4
+		case 'K':
+			score += 5
+		case 'J', 'X':
+			score += 8
+		case 'Q', 'Z':
+			score += 10
+		}
+	}
+	return
 }
