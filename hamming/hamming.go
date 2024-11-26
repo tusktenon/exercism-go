c94fa16c3d4d99c7124ca98356e27f379863fbdd
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-25 17:10:18 -0800

    Hamming: complete

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,5 +1,16 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if len(a) != len(b) {
+		return 0, errors.New("strings have different lengths")
+	}
+	d := 0
+	for i := range a {
+		if a[i] != b[i] {
+			d++
+		}
+	}
+	return d, nil
 }
