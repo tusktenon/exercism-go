71689ff8325eafb9b7e196da1e265d1b43e1ae9c
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-25 20:58:07 -0800

    Isogram: bit-flag solution

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,5 +1,21 @@
 package isogram
 
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+	var seen, flag int32
+	for _, b := range []byte(word) {
+		flag = 0
+		if 'a' <= b && b <= 'z' {
+			flag = 1 << (b - 'a')
+		} else if 'A' <= b && b <= 'Z' {
+			flag = 1 << (b - 'A')
+		}
+		if flag != 0 {
+			if seen&flag != 0 {
+				return false
+			} else {
+				seen |= flag
+			}
+		}
+	}
+	return true
 }
