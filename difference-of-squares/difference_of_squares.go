71c29b533096cc1829273d39a123cfc9259ccf34
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-27 21:52:35 -0800

    Difference of Squares: first iteration

--- difference-of-squares/difference_of_squares.go
+++ difference-of-squares/difference_of_squares.go
@@ -1,13 +1,14 @@
 package diffsquares
 
 func SquareOfSum(n int) int {
-	panic("Please implement the SquareOfSum function")
+	s := n * (n + 1) / 2
+	return s * s
 }
 
 func SumOfSquares(n int) int {
-	panic("Please implement the SumOfSquares function")
+	return n * (n + 1) * (2*n + 1) / 6
 }
 
 func Difference(n int) int {
-	panic("Please implement the Difference function")
+	return SquareOfSum(n) - SumOfSquares(n)
 }
