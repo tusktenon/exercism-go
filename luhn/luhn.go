0fad50f565c52353dfb91feac5ece34973cfd331
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-28 20:13:06 -0800

    Luhn: first iteration

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,5 +1,24 @@
 package luhn
 
 func Valid(id string) bool {
-	panic("Please implement the Valid function")
+	var luhn, count int
+	for i := len(id) - 1; i >= 0; i-- {
+		switch c := id[i]; {
+		case '0' <= c && c <= '9':
+			count++
+			d := int(c - '0')
+			if count&1 == 0 {
+				d *= 2
+				if d > 9 {
+					d -= 9
+				}
+			}
+			luhn += d
+		case c == ' ':
+			continue
+		default:
+			return false
+		}
+	}
+	return count > 1 && luhn%10 == 0
 }
