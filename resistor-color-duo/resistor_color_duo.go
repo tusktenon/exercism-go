e2d02cdcb27504cb0c971dc8f585372c3fc46547
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-11-28 22:06:12 -0800

    Resistor Color Duo: complete

--- resistor-color-duo/resistor_color_duo.go
+++ resistor-color-duo/resistor_color_duo.go
@@ -1,6 +1,19 @@
 package resistorcolorduo
 
+var colorValue map[string]int = map[string]int{
+	"black":  0,
+	"brown":  1,
+	"red":    2,
+	"orange": 3,
+	"yellow": 4,
+	"green":  5,
+	"blue":   6,
+	"violet": 7,
+	"grey":   8,
+	"white":  9,
+}
+
 // Value should return the resistance value of a resistor with a given colors.
 func Value(colors []string) int {
-	panic("Implement the Value function")
+	return 10*colorValue[colors[0]] + colorValue[colors[1]]
 }
