42a00050d8b3688bf3469afa2a6bbf847585010b
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-12-02 17:28:12 -0500

    Sieve: first iteration

--- sieve/sieve.go
+++ sieve/sieve.go
@@ -1,5 +1,20 @@
 package sieve
 
 func Sieve(limit int) []int {
-	panic("Please implement the Sieve function")
+	marked := make([]bool, limit+1)
+	marked[0], marked[1] = true, true
+	for i := 2; i*i <= limit; i++ {
+		if !marked[i] {
+			for j := i * i; j <= limit; j += i {
+				marked[j] = true
+			}
+		}
+	}
+	primes := []int{}
+	for i, m := range marked {
+		if !m {
+			primes = append(primes, i)
+		}
+	}
+	return primes
 }
