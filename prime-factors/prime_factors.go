4ee1e539011a2ef69b294f33ec51fe28315c1aea
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-12-03 11:01:40 -0500

    Prime Factors: second iteration

--- prime-factors/prime_factors.go
+++ prime-factors/prime_factors.go
@@ -1,13 +1,17 @@
 package prime
 
 func Factors(n int64) (factors []int64) {
-    for d := int64(2); n > 1; {
+	// We can double the function's speed (verified with benchmarking) by only
+	// considering odd numbers as possible factors after first considering 2.
+	var d, step int64 = 2, 1
+	for n > 1 {
 		if n%d == 0 {
 			factors = append(factors, d)
 			n /= d
 		} else {
-            d++
-        }
+			d += step
+			step = 2
+		}
 	}
 	return
 }
