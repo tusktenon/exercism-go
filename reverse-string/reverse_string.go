33253145f052b4888f5963673ed128d3bfc6abcf
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-12-03 12:15:49 -0500

    Reverse String: first iteration

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -1,5 +1,10 @@
 package reverse
 
 func Reverse(input string) string {
-	panic("Please implement the Reverse function")
+	runes := []rune(input)
+	n := len(runes)
+	for i := 0; i < n/2; i++ {
+		runes[i], runes[n-i-1] = runes[n-i-1], runes[i]
+	}
+	return string(runes)
 }
