f5c63928085c5505a76ecf186a23c3d9c74fe896
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-12-03 10:53:27 -0500

    Prime Factors: first iteration

--- prime-factors/prime_factors.go
+++ prime-factors/prime_factors.go
@@ -1,5 +1,13 @@
 package prime
 
-func Factors(n int64) []int64 {
-	panic("Please implement the Factors function")
+func Factors(n int64) (factors []int64) {
+    for d := int64(2); n > 1; {
+		if n%d == 0 {
+			factors = append(factors, d)
+			n /= d
+		} else {
+            d++
+        }
+	}
+	return
 }
