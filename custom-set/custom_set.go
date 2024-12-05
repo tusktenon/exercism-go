be5eda73a788c61676e5951641c10adbaafc830f
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-12-05 10:12:10 -0500

    Custom Set: sorted slice approach

--- custom-set/custom_set.go
+++ custom-set/custom_set.go
@@ -1,58 +1,173 @@
+// Package stringset implements a Set as a collection of unique string values.
 package stringset
 
-// Implement Set as a collection of unique string values.
-//
-// For Set.String, use '{' and '}', output elements as double-quoted strings
-// safely escaped with Go syntax, and use a comma and a single space between
-// elements. For example, a set with 2 elements, "a" and "b", should be formatted as {"a", "b"}.
-// Format the empty set as {}.
+import (
+	"fmt"
+	"reflect"
+	"slices"
+	"sort"
+	"strings"
+)
 
-// Define the Set type here.
+// Implement the Set type using a sorted string slice.
+type Set struct{ elements []string }
 
 func New() Set {
-	panic("Please implement the New function")
+	return Set{elements: []string{}}
 }
 
 func NewFromSlice(l []string) Set {
-	panic("Please implement the NewFromSlice function")
+	// work from a copy to avoid mutating the source slice
+	c := make([]string, len(l))
+	copy(c, l)
+	if len(c) < 2 {
+		return Set{elements: c}
+	}
+	sort.Strings(c)
+	prev := c[0]
+	dedup := []string{prev}
+	for _, curr := range c[1:] {
+		if curr != prev {
+			dedup = append(dedup, curr)
+		}
+		prev = curr
+	}
+	return Set{elements: dedup}
 }
 
+// For Set.String, use '{' and '}', output elements as double-quoted strings
+// safely escaped with Go syntax, and use a comma and a single space between
+// elements. For example, a set with 2 elements, "a" and "b", should be
+// formatted as {"a", "b"}. Format the empty set as {}.
 func (s Set) String() string {
-	panic("Please implement the String function")
+	var b strings.Builder
+	b.WriteByte('{')
+	prefix := ""
+	for _, e := range s.elements {
+		fmt.Fprintf(&b, "%s\"%s\"", prefix, e)
+		prefix = ", "
+	}
+	b.WriteByte('}')
+	return b.String()
 }
 
 func (s Set) IsEmpty() bool {
-	panic("Please implement the IsEmpty function")
+	return len(s.elements) == 0
 }
 
 func (s Set) Has(elem string) bool {
-	panic("Please implement the Has function")
+	_, found := slices.BinarySearch(s.elements, elem)
+	return found
 }
 
-func (s Set) Add(elem string) {
-	panic("Please implement the Add function")
+// NOTE: Add must take a pointer receiver to be able to modify s.elements. In a
+// realistic Go program, convention dictates that if any method has a pointer
+// receiver, then they all should. Since none of the other Set methods modify
+// their receiver, a better solution would be to have Add take its receiver by
+// value and return the modified Set (in analogy with the built-in append
+// function, slices.Insert, etc.).
+func (s *Set) Add(elem string) {
+	if i, found := slices.BinarySearch(s.elements, elem); !found {
+		s.elements = slices.Insert(s.elements, i, elem)
+	}
 }
 
 func Subset(s1, s2 Set) bool {
-	panic("Please implement the Subset function")
+	if len(s1.elements) > len(s2.elements) {
+		return false
+	}
+	var i, j int
+	for i < len(s1.elements) && j < len(s2.elements) {
+		e1, e2 := s1.elements[i], s2.elements[j]
+		switch {
+		case e1 < e2:
+			return false
+		case e1 == e2:
+			i++
+			j++
+		case e1 > e2:
+			j++
+		}
+	}
+	return i == len(s1.elements)
 }
 
 func Disjoint(s1, s2 Set) bool {
-	panic("Please implement the Disjoint function")
+	for i, j := 0, 0; i < len(s1.elements) && j < len(s2.elements); {
+		e1, e2 := s1.elements[i], s2.elements[j]
+		switch {
+		case e1 < e2:
+			i++
+		case e1 == e2:
+			return false
+		case e1 > e2:
+			j++
+		}
+	}
+	return true
 }
 
 func Equal(s1, s2 Set) bool {
-	panic("Please implement the Equal function")
+	return reflect.DeepEqual(s1.elements, s2.elements)
 }
 
 func Intersection(s1, s2 Set) Set {
-	panic("Please implement the Intersection function")
+	common := []string{}
+	for i, j := 0, 0; i < len(s1.elements) && j < len(s2.elements); {
+		e1, e2 := s1.elements[i], s2.elements[j]
+		switch {
+		case e1 < e2:
+			i++
+		case e1 == e2:
+			common = append(common, e1)
+			i++
+			j++
+		case e1 > e2:
+			j++
+		}
+	}
+	return Set{elements: common}
 }
 
 func Difference(s1, s2 Set) Set {
-	panic("Please implement the Difference function")
+	diff := []string{}
+	var i, j int
+	for i < len(s1.elements) && j < len(s2.elements) {
+		e1, e2 := s1.elements[i], s2.elements[j]
+		switch {
+		case e1 < e2:
+			diff = append(diff, e1)
+			i++
+		case e1 == e2:
+			i++
+			j++
+		case e1 > e2:
+			j++
+		}
+	}
+	diff = append(diff, s1.elements[i:]...)
+	return Set{elements: diff}
 }
 
 func Union(s1, s2 Set) Set {
-	panic("Please implement the Union function")
+	either := make([]string, 0, max(len(s1.elements), len(s2.elements)))
+	var i, j int
+	for i < len(s1.elements) && j < len(s2.elements) {
+		e1, e2 := s1.elements[i], s2.elements[j]
+		switch {
+		case e1 < e2:
+			either = append(either, e1)
+			i++
+		case e1 == e2:
+			either = append(either, e1)
+			i++
+			j++
+		case e1 > e2:
+			either = append(either, e2)
+			j++
+		}
+	}
+	either = append(either, s1.elements[i:]...)
+	either = append(either, s2.elements[j:]...)
+	return Set{elements: either}
 }
