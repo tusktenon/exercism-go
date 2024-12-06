93011988399f2d0f2bffa298ec6c9beeb4037e18
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-12-06 10:02:44 -0500

    Roman Numerals: switch from slices to arrays
    Using explicitly-sized arrays feels more correct and helps guard against
    size mismatches.

--- roman-numerals/roman_numerals.go
+++ roman-numerals/roman_numerals.go
@@ -6,8 +6,8 @@ import (
 )
 
 var (
-	arabic = []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
-	roman  = []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+	arabic = [13]int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	roman  = [13]string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
 )
 
 func ToRomanNumeral(n int) (string, error) {
