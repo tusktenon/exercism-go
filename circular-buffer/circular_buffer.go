95b6dc6261a62847abe035f7aa9f89064122caf7
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-12-06 14:40:23 -0500

    Circular Buffer: first iteration

--- circular-buffer/circular_buffer.go
+++ circular-buffer/circular_buffer.go
@@ -1,30 +1,51 @@
+// Package circular provides a circular buffer of bytes supporting both
+// overflow-checked writes and unconditional, possibly overwriting, writes.
+//
+// We chose the provided API so that Buffer implements io.ByteReader and
+// io.ByteWriter and can be used (size permitting) as a drop in replacement for
+// anything using that interface.
 package circular
 
-// Implement a circular buffer of bytes supporting both overflow-checked writes
-// and unconditional, possibly overwriting, writes.
-//
-// We chose the provided API so that Buffer implements io.ByteReader
-// and io.ByteWriter and can be used (size permitting) as a drop in
-// replacement for anything using that interface.
+import "errors"
 
-// Define the Buffer type here.
+type Buffer struct {
+	data        []byte
+	start, used int
+}
 
 func NewBuffer(size int) *Buffer {
-	panic("Please implement the NewBuffer function")
+	return &Buffer{data: make([]byte, size)}
 }
 
 func (b *Buffer) ReadByte() (byte, error) {
-	panic("Please implement the ReadByte function")
+	if b.used == 0 {
+		return 0, errors.New("buffer is empty")
+	}
+	val := b.data[b.start]
+	b.start = (b.start + 1) % len(b.data)
+	b.used--
+	return val, nil
 }
 
 func (b *Buffer) WriteByte(c byte) error {
-	panic("Please implement the WriteByte function")
+	if b.used == len(b.data) {
+		return errors.New("buffer is full")
+	}
+	b.data[(b.start+b.used)%len(b.data)] = c
+	b.used++
+	return nil
 }
 
 func (b *Buffer) Overwrite(c byte) {
-	panic("Please implement the Overwrite function")
+	if b.used == len(b.data) {
+		b.data[b.start] = c
+		b.start = (b.start + 1) % len(b.data)
+	} else {
+		b.data[(b.start+b.used)%len(b.data)] = c
+		b.used++
+	}
 }
 
 func (b *Buffer) Reset() {
-	panic("Please implement the Reset function")
+	b.start, b.used = 0, 0
 }
