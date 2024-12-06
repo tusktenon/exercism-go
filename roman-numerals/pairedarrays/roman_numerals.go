c9cff383533209a4ae97c2ef288a1241ee171d9f
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-12-06 10:06:53 -0500

    Roman Numerals: save first solution

---
+++ roman-numerals/pairedarrays/roman_numerals.go
@@ -0,0 +1,24 @@
+package romannumerals
+
+import (
+	"fmt"
+	"strings"
+)
+
+var (
+	arabic = [13]int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	roman  = [13]string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+)
+
+func ToRomanNumeral(n int) (string, error) {
+	if n < 1 || n > 3999 {
+		return "", fmt.Errorf("out of range: %d", n)
+	}
+	var b strings.Builder
+	for i, a := range arabic {
+		for ; n >= a; n -= a {
+			b.WriteString(roman[i])
+		}
+	}
+	return b.String(), nil
+}
