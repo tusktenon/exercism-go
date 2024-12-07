6f202d6b20263acf7faf77740d520c2b5cef654c
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-12-07 13:42:41 -0500

    Binary Search: define mid to avoid overflow

--- binary-search/binary_search.go
+++ binary-search/binary_search.go
@@ -2,7 +2,7 @@ package binarysearch
 
 func SearchInts(list []int, key int) int {
 	for left, right := 0, len(list)-1; left <= right; {
-		switch mid := (left + right) / 2; {
+		switch mid := left + (right-left)/2; {
 		case list[mid] < key:
 			left = mid + 1
 		case list[mid] == key:
