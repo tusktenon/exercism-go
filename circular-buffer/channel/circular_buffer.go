b8746e5ab69ad249bb70709e306a79dac86463a2
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-12-08 15:42:16 -0500

    Circular Buffer: add buffered channel approach

---
+++ circular-buffer/channel/circular_buffer.go
@@ -0,0 +1,43 @@
+// An implementation using a buffered channel. This makes for very concise and
+// readable code, but is probably unwise all the same. Note that "The Go
+// Programming Languages" (p. 233) specifically warns against using a buffered
+// channel as a queue, and recommends a slice-based implementation instead.
+package circular
+
+import "errors"
+
+type Buffer struct {
+	data chan byte
+}
+
+func NewBuffer(size int) *Buffer {
+	return &Buffer{data: make(chan byte, size)}
+}
+
+func (b *Buffer) ReadByte() (byte, error) {
+	if len(b.data) == 0 {
+		return 0, errors.New("buffer is empty")
+	}
+	return <-b.data, nil
+}
+
+func (b *Buffer) WriteByte(c byte) error {
+	if len(b.data) == cap(b.data) {
+		return errors.New("buffer is full")
+	}
+	b.data <- c
+	return nil
+}
+
+func (b *Buffer) Overwrite(c byte) {
+	if len(b.data) == cap(b.data) {
+		<-b.data
+	}
+	b.data <- c
+}
+
+func (b *Buffer) Reset() {
+	for len(b.data) != 0 {
+		<-b.data
+	}
+}
