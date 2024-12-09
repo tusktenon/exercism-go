6f192e8fb6b577d52023b4051ddaef40c1ae7e72
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-12-09 11:07:20 -0500

    Flatten Array: use the `any` alias

--- flatten-array/flatten_array.go
+++ flatten-array/flatten_array.go
@@ -1,6 +1,6 @@
 package flatten
 
-func Flatten(nested interface{}) []interface{} {
+func Flatten(nested any) []any {
 	// allow unused implementation
 	_, _ = flatten1, flatten2
 
@@ -10,32 +10,32 @@ func Flatten(nested interface{}) []interface{} {
 
 // Option 1: A "pure" functional approach (no mutation).
 // Runs slower, uses more memory and performs more allocations.
-func flatten1(nested interface{}) []interface{} {
+func flatten1(nested any) []any {
 	switch nested := nested.(type) {
 	case nil:
-		return []interface{}{}
-	case []interface{}:
+		return []any{}
+	case []any:
 		if len(nested) == 0 {
-			return []interface{}{}
+			return []any{}
 		}
 		return append(Flatten(nested[0]), Flatten(nested[1:])...)
 	default:
-		return []interface{}{nested}
+		return []any{nested}
 	}
 }
 
 // Option 2: Combine recursion and iteration.
-func flatten2(nested interface{}) []interface{} {
+func flatten2(nested any) []any {
 	switch nested := nested.(type) {
 	case nil:
-		return []interface{}{}
-	case []interface{}:
-		flattened := []interface{}{}
+		return []any{}
+	case []any:
+		flattened := []any{}
 		for _, e := range nested {
 			flattened = append(flattened, flatten2(e)...)
 		}
 		return flattened
 	default:
-		return []interface{}{nested}
+		return []any{nested}
 	}
 }
