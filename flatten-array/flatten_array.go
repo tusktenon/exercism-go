dcc5aba5b8a0b898e52e4af83fe883f69db5fd00
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-12-09 08:54:33 -0500

    Flatten Array: first iteration

--- flatten-array/flatten_array.go
+++ flatten-array/flatten_array.go
@@ -1,5 +1,14 @@
 package flatten
 
 func Flatten(nested interface{}) []interface{} {
-	panic("Please implement the Flatten function")
+	if nested == nil {
+		return []interface{}{}
+	}
+	if s, ok := nested.([]interface{}); ok {
+		if len(s) == 0 {
+			return []interface{}{}
+		}
+		return append(Flatten(s[0]), Flatten(s[1:])...)
+	}
+	return []interface{}{nested}
 }
