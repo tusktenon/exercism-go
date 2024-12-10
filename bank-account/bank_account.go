13e67f711641454a21f23e8d12f30c40c3b118a8
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2024-12-10 10:16:00 -0500

    Bank Account: first iteration

--- bank-account/bank_account.go
+++ bank-account/bank_account.go
@@ -1,19 +1,51 @@
 package account
 
-// Define the Account type here.
+import "sync"
+
+type Account struct {
+	mu      sync.Mutex // guards account
+	balance int64
+	closed  bool
+}
 
 func Open(amount int64) *Account {
-	panic("Please implement the Open function")
+	if amount < 0 {
+		return nil
+	}
+	return &Account{balance: amount}
 }
 
 func (a *Account) Balance() (int64, bool) {
-	panic("Please implement the Balance function")
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	if a.closed {
+		return 0, false
+	}
+	return a.balance, true
 }
 
 func (a *Account) Deposit(amount int64) (int64, bool) {
-	panic("Please implement the Deposit function")
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	if a.closed {
+		return 0, false
+	}
+	newBalance := a.balance + amount
+	if newBalance < 0 {
+		return a.balance, false
+	}
+	a.balance = newBalance
+	return a.balance, true
 }
 
 func (a *Account) Close() (int64, bool) {
-	panic("Please implement the Close function")
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	if a.closed {
+		return 0, false
+	}
+	closingBalance := a.balance
+	a.balance = 0
+	a.closed = true
+	return closingBalance, true
 }
