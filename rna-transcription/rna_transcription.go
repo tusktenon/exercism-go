dd84563d37ef727dd38ce70b5fd67d545532a46a
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-01-03 13:18:58 -0500

    RNA Transcription: add alternate solutions

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,6 +1,14 @@
 package strand
 
+import "strings"
+
 func ToRNA(dna string) string {
+	_, _, _, _ = toRNA1, toRNA2, toRNA3, toRNA4
+	return toRNA1(dna)
+}
+
+// Option 1: Use a switch statement.
+func toRNA1(dna string) string {
 	strand := []byte(dna)
 	for i, n := range strand {
 		switch n {
@@ -13,8 +21,43 @@ func ToRNA(dna string) string {
 		case 'T':
 			strand[i] = 'A'
 		default:
-			panic("Invalid nucleotide symbol")
+			panic("invalid nucleotide symbol")
 		}
+	}
+	return string(strand)
+}
+
+// Option 2: Use string search and indexing.
+// A little more compact than Option 1, but also slower.
+func toRNA2(dna string) string {
+	strand := []byte(dna)
+	for i, n := range strand {
+		strand[i] = "UGCA"[strings.IndexByte("ACGT", n)]
+	}
+	return string(strand)
+}
+
+// Option 3: Use the strings.Map function.
+// Elegant, but also slower than the previous options.
+func toRNA3(dna string) string {
+	translate := func(r rune) rune {
+		return rune("UGCA"[strings.IndexRune("ACGT", r)])
+	}
+	return strings.Map(translate, dna)
+}
+
+// Option 4: Use a map.
+// As expected, this is by far the slowest option.
+func toRNA4(dna string) string {
+	translate := map[byte]byte{
+		'A': 'U',
+		'C': 'G',
+		'G': 'C',
+		'T': 'A',
+	}
+	strand := []byte(dna)
+	for i, n := range strand {
+		strand[i] = translate[n]
 	}
 	return string(strand)
 }
