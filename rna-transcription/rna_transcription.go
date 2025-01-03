5a11337a4cccb28cf033c7057647205437cd4139
Dan Murphy <dan.murphy@alumni.utoronto.ca>
2025-01-03 12:45:43 -0500

    RNA Transcription: first iteration

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,5 +1,20 @@
 package strand
 
 func ToRNA(dna string) string {
-	panic("Please implement the ToRNA function")
+	strand := []byte(dna)
+	for i, n := range strand {
+		switch n {
+		case 'A':
+			strand[i] = 'U'
+		case 'C':
+			strand[i] = 'G'
+		case 'G':
+			strand[i] = 'C'
+		case 'T':
+			strand[i] = 'A'
+		default:
+			panic("Invalid nucleotide symbol")
+		}
+	}
+	return string(strand)
 }
